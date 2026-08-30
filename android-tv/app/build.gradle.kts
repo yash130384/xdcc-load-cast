d63fc2187e326cbc563a21f0ebf2d7ecda677ebd
Yash <yash@users.noreply.github.com>
2026-08-30 16:12:34 +0200

    chore: bump app version to 1.1.0 and release updated APK for auto-updater

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pulsecast.tv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
